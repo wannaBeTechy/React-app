d5b639894c23b3d94c7ec6f62fe993e6eabef01d
Mohammed Sumairuddin <68439115+wannaBeTechy@users.noreply.github.com>
2021-04-11 16:44:58 +0530

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <img src='https://travel.mqcdn.com/mapquest/travel/wp-content/uploads/2020/06/GettyImages-1211212607.jpg' />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
